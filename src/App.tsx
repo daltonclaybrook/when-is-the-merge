dff830d9e32125c3124ac633b39406f94f8eb4dd
Dalton Claybrook <daltonclaybrook@gmail.com>
2022-08-24 21:18:07 -0400

    Initial site

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import { FC } from 'react';
 import './App.css';
+import { useEstimatedMergeInfo, EstimatedMergeInfo } from './useEstimatedMergeInfo';
 
-function App() {
+const App: FC = () => {
+    const mergeInfo = useEstimatedMergeInfo();
+
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
+            <h1>
+                When is <i>The Merge</i>?
+            </h1>
+            {mergeInfo && <MergeInfo {...mergeInfo} />}
+            {!mergeInfo && <p>Loading...</p>}
         </div>
     );
-}
+};
+
+const MergeInfo: FC<EstimatedMergeInfo> = ({
+    latestBlockNumber,
+    latestTotalDifficulty,
+    terminalTotalDifficulty,
+    estimatedMergeBlockNumber,
+    estimatedMergeDate,
+}) => (
+    <div>
+        <h2>{dateString(estimatedMergeDate)}</h2>
+        <hr />
+        <h3>{latestBlockNumber}</h3>
+        <p>latest block number</p>
+        <hr />
+        <h3>{estimatedMergeBlockNumber}</h3>
+        <p>estimated merge block number</p>
+        <hr />
+        <h3>{formatNumber(latestTotalDifficulty)}</h3>
+        <p>latest total difficulty</p>
+        <hr />
+        <h3>{formatNumber(terminalTotalDifficulty)}</h3>
+        <p>terminal total difficulty</p>
+    </div>
+);
+
+const dateString = (timestamp: number) => {
+    const date = new Date(timestamp * 1000);
+    return date.toLocaleString();
+};
+
+const formatNumber = (string: string) => {
+    const formatter = new Intl.NumberFormat();
+    return formatter.format(BigInt(string));
+};
 
 export default App;
---
+++ src/useEstimatedMergeInfo.ts
@@ -0,0 +1,35 @@
+import axios, { AxiosResponse } from 'axios';
+import { useEffect, useState } from 'react';
+
+const apiBaseURL = process.env.REACT_APP_API_BASE_URL as string;
+
+export interface EstimatedMergeInfo {
+    /// The latest block number
+    latestBlockNumber: string;
+    /// The total difficulty as of the latest block
+    latestTotalDifficulty: string;
+    /// The total difficulty that will trigger The Merge
+    terminalTotalDifficulty: string;
+    /// The estimated block number of the merge block
+    estimatedMergeBlockNumber: string;
+    /// The estimated date of The Merge, in unix seconds
+    estimatedMergeDate: number;
+}
+
+export const useEstimatedMergeInfo = () => {
+    const [mergeInfo, setMergeInfo] = useState<EstimatedMergeInfo | null>(null);
+    useEffect(() => {
+        fetchEstimatedMergeInfo().then(setMergeInfo);
+    }, []);
+    return mergeInfo;
+};
+
+const fetchEstimatedMergeInfo = async (): Promise<EstimatedMergeInfo | null> => {
+    try {
+        const response: AxiosResponse<EstimatedMergeInfo> = await axios.get(apiBaseURL);
+        return response.data;
+    } catch (err) {
+        console.error(`Error fetching estimated merge info: ${err}`);
+        return null;
+    }
+};
