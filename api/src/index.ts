e2be5476bf34f533b1815fa1314fb7d5759c47bc
Dalton Claybrook <daltonclaybrook@gmail.com>
2022-08-24 20:49:20 -0400

    Add cache control header

--- api/src/index.ts
+++ api/src/index.ts
@@ -9,6 +9,12 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '1mb' }));
 
+app.use((req, res, next) => {
+    // Results stay cached for 20 seconds
+    res.set('Cache-Control', 'public, max-age=20');
+    next();
+});
+
 app.get('/', async (req, res) => {
     try {
         const info = await fetchEstimatedMergeInfo();
