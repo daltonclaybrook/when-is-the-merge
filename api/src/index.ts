35fd055784d722582be0b58788ba9ae806fe5c28
Dalton Claybrook <daltonclaybrook@gmail.com>
2022-09-07 18:49:55 -0400

    Increase max age to 120

--- api/src/index.ts
+++ api/src/index.ts
@@ -10,8 +10,8 @@ app.use(cors());
 app.use(express.json({ limit: '1mb' }));
 
 app.use((req, res, next) => {
-    // Results stay cached for 20 seconds
-    res.set('Cache-Control', 'public, max-age=20');
+    // Results stay cached for 120 seconds
+    res.set('Cache-Control', 'public, max-age=120');
     next();
 });
 
